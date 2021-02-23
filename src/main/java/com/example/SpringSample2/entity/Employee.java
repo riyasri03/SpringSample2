032f8c5dd03aead6b41fd69713ae765808f96879
Riya Srivastava <riyasrivastava@Riyas-MacBook-Pro.local>
2021-02-23 17:49:43 +0530

    mongo added

--- src/main/java/com/example/SpringSample2/entity/Employee.java
+++ src/main/java/com/example/SpringSample2/entity/Employee.java
@@ -2,23 +2,26 @@ package com.example.SpringSample2.entity;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.hibernate.annotations.GenericGenerator;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.MongoId;
+//import org.hibernate.annotations.GenericGenerator;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-@Entity
+//import javax.persistence.Entity;
+//import javax.persistence.GeneratedValue;
+//import javax.persistence.GenerationType;
+//import javax.persistence.Id;
+//@Entity
+@Document
 @Getter
 @Setter
 public class Employee {
-    @Id
+//    @Id
+//
+//
+//    @GenericGenerator(name = "employee_id_seq", strategy = "increment")
+//    @GeneratedValue(generator = "employee_id_seq", strategy = GenerationType.AUTO)
 
-
-    @GenericGenerator(name = "employee_id_seq", strategy = "increment")
-    @GeneratedValue(generator = "employee_id_seq", strategy = GenerationType.AUTO)
-
-
+ @MongoId
 
     private long id;
     private String name;
