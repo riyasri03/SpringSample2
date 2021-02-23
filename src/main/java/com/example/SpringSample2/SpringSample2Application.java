965c1e1d03a911d1eba3f37d9dceb1b4b6c1b713
Riya Srivastava <riyasrivastava@Riyas-MacBook-Pro.local>
2021-02-23 14:15:44 +0530

    First commit

---
+++ src/main/java/com/example/SpringSample2/SpringSample2Application.java
@@ -0,0 +1,13 @@
+package com.example.SpringSample2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSample2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSample2Application.class, args);
+	}
+
+}
